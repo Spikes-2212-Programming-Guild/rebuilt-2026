c0e2f6288aba173e6052b91cba47e562c9388a7c
itay <itay2318@gmail.com>
2026-01-16 21:41:16 +0200

    added the utility StandardDeviations.java to make working with Matrices easier

---
+++ src/main/java/frc/robot/util/odometry/StandardDeviations.java
@@ -0,0 +1,17 @@
+package frc.robot.util.odometry;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+
+public record StandardDeviations(double translationStdDev, double rotationStdDev) {
+
+    public Matrix<N3, N1> toMatrix() {
+        return VecBuilder.fill(
+                translationStdDev,
+                translationStdDev,
+                rotationStdDev
+        );
+    }
+}
