2554bfe32759c571063d1365a67d074191f5f055
itay <itay2318@gmail.com>
2026-01-18 08:58:11 +0200

    made PeriodicTaskScheduler.java not a singleton, to fix the problem with the init method

--- src/main/java/frc/robot/util/odometry/PeriodicTaskScheduler.java
+++ src/main/java/frc/robot/util/odometry/PeriodicTaskScheduler.java
@@ -4,22 +4,10 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 public class PeriodicTaskScheduler {
 
-    private static volatile PeriodicTaskScheduler instance;
     private final TimedRobot timedRobot;
 
-    private PeriodicTaskScheduler(TimedRobot timedRobot) {
+    public PeriodicTaskScheduler(TimedRobot timedRobot) {
         this.timedRobot = timedRobot;
-    }
-
-    public static void init(TimedRobot timedRobot) {
-        if (instance == null) {
-            instance = new PeriodicTaskScheduler(timedRobot);
-        }
-    }
-
-    // TODO - consider adding a throw if instance is null
-    public static PeriodicTaskScheduler getInstance() {
-        return instance;
     }
 
     public void schedule(Runnable task, double frequencyHz, double delaySeconds) {
