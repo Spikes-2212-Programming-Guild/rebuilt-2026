d7508351c540c4b3bb5e0ab09b72cc9e315e33ab
adiY666 <adi.yekutieljr@gmail.com>
2026-01-18 17:24:19 +0200

    made more accurate name to FindHoodLimits.java

--- src/main/java/frc/robot/commands/FindHoodLimit.java
+++ src/main/java/frc/robot/commands/FindHoodLimit.java
@@ -3,11 +3,11 @@ package frc.robot.commands;
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import frc.robot.subsystems.Hood;
 
-public class FindHoodLimits extends MoveGenericSubsystem {
+public class FindHoodLimit extends MoveGenericSubsystem {
 
     private static final double HOOD_RESET0_SPEED = -1.0;
 
-    public FindHoodLimits(Hood hood) {
+    public FindHoodLimit(Hood hood) {
         super(hood, HOOD_RESET0_SPEED);
         addRequirements(hood);
     }
