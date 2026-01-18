141ea2fc0426e972f8e2733563afa5631994c534
itay <itay2318@gmail.com>
2026-01-18 10:09:02 +0200

    added resetRotation method

--- src/main/java/frc/robot/util/localization/RobotPoseEstimator.java
+++ src/main/java/frc/robot/util/localization/RobotPoseEstimator.java
@@ -58,6 +58,10 @@ public class RobotPoseEstimator {
         poseEstimator.resetPose(newPose);
     }
 
+    public void resetRotation(Rotation2d newAngle) {
+        poseEstimator.resetRotation(newAngle);
+    }
+
     public SwerveDrivePoseEstimator getEstimator() {
         return poseEstimator;
     }
