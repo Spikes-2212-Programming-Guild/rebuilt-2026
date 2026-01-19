93fcf2e2d250b782c2b7b3734f8d2be3dc8d280f
ofek <ofek@ofek-online.com>
2026-01-19 13:39:01 +0200

    merged two commands into one

--- src/main/java/frc/robot/commands/MoveCollectionUp.java
+++ src/main/java/frc/robot/commands/MoveCollectionUp.java
@@ -1,24 +1,23 @@
 package frc.robot.commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
-import subsystems.CollectionMovement;
+import frc.robot.subsystems.CollectionMovement;
 
 import java.util.function.Supplier;
 
 public class MoveCollectionUp extends MoveGenericSubsystem {
 
     private CollectionMovement collection;
+    private Supplier<Double> speedSupplier;
 
     public MoveCollectionUp(CollectionMovement collection, Supplier<Double> speedSupplier) {
         super(collection, speedSupplier);
         this.collection = collection;
+        this.speedSupplier = speedSupplier;
     }
 
     @Override
     public boolean isFinished() {
-        if(collection.isAtTopLimitSwitch()){
-            return true;
-        }
-        return false;
+        return collection.canMove(speedSupplier.get());
     }
 }
