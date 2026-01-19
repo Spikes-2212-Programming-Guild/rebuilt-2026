14d68e2a6da86f4c059fb888e372d82bb7a3854e
ofek <ofek@ofek-online.com>
2026-01-19 16:22:41 +0200

    fixed robotmap in memorial of itay

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,10 +3,12 @@ package frc.robot;
 public class RobotMap {
 
     public interface CAN {
+
         int TALON_COLLECTION_MOVEMENT_ID = -1;
     }
     
     public interface DIO {
+
         int TOP_LIMIT_SWITCH_ID = -1;
         int BOTTOM_LIMIT_SWITCH_ID = -1;
     }
