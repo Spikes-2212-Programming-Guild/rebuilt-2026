af01736506d5906e66e57ab40a0cb0c1acff0055
ofek <ofek@ofek-online.com>
2026-01-19 16:17:18 +0200

    made subsystem singelton

--- src/main/java/frc/robot/subsystems/CollectionMove.java
+++ src/main/java/frc/robot/subsystems/CollectionMove.java
@@ -1,15 +1,31 @@
 package frc.robot.subsystems;
 
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
+import com.spikes2212.util.smartmotorcontrollers.TalonFXWrapper;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+
+import static frc.robot.RobotMap.CAN.TALON_COLLECTION_MOVEMENT_ID;
+import static frc.robot.RobotMap.DIO.BOTTOM_LIMIT_SWITCH_ID;
+import static frc.robot.RobotMap.DIO.TOP_LIMIT_SWITCH_ID;
 
 public class CollectionMove extends MotoredGenericSubsystem {
 
     private final DigitalInput topLimitSwitch;
     private final DigitalInput bottomLimitSwitch;
+    private static CollectionMove instance;
 
-    public CollectionMove(String namespaceName, DigitalInput topLimitSwitch, DigitalInput bottomLimitSwitch,
+    public static CollectionMove getInstance(){
+        if(instance == null){
+            instance = new CollectionMove("collection movement",
+                    new DigitalInput(TOP_LIMIT_SWITCH_ID),
+                    new DigitalInput(BOTTOM_LIMIT_SWITCH_ID),
+                    new TalonFXWrapper(TALON_COLLECTION_MOVEMENT_ID));
+        }
+        return instance;
+    }
+
+    private CollectionMove(String namespaceName, DigitalInput topLimitSwitch, DigitalInput bottomLimitSwitch,
                           MotorController... motorControllers) {
         super(namespaceName, motorControllers);
         this.topLimitSwitch = topLimitSwitch;
