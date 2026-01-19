45526ca55036b5a2b159a37063cf34c3263c15fe
ofek <ofek@ofek-online.com>
2026-01-19 16:21:11 +0200

    removed class fields and adapted

--- src/main/java/frc/robot/commands/MoveCollection.java
+++ src/main/java/frc/robot/commands/MoveCollection.java
@@ -7,17 +7,12 @@ import java.util.function.Supplier;
 
 public class MoveCollection extends MoveGenericSubsystem {
 
-    private final CollectionMovement collection;
-    private final Supplier<Double> speedSupplier;
-
     public MoveCollection(CollectionMovement collection, Supplier<Double> speedSupplier) {
         super(collection, speedSupplier);
-        this.collection = collection;
-        this.speedSupplier = speedSupplier;
     }
 
     @Override
     public boolean isFinished() {
-        return collection.canMove(speedSupplier.get());
+        return super.subsystem.canMove(speedSupplier.get());
     }
 }
