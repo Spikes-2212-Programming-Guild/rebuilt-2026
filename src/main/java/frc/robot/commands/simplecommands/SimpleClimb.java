a863b276051375311176b2d9b110b0dbf88a10db
itay <itay2318@gmail.com>
2026-01-20 10:10:21 +0200

    changed simpleClimb to use climb instead of a generic subsystem

--- src/main/java/frc/robot/commands/simplecommands/SimpleClimb.java
+++ src/main/java/frc/robot/commands/simplecommands/SimpleClimb.java
@@ -1,13 +1,13 @@
 package frc.robot.commands.simplecommands;
 
-import com.spikes2212.command.genericsubsystem.GenericSubsystem;
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import frc.robot.subsystems.Climb;
 
 import java.util.function.Supplier;
 
 public class SimpleClimb extends MoveGenericSubsystem {
 
-    public SimpleClimb(GenericSubsystem subsystem, Supplier<Double> speed) {
-        super(subsystem, speed);
+    public SimpleClimb(Climb climb, Supplier<Double> speed) {
+        super(climb, speed);
     }
 }
