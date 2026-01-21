79a8ca9ef884eb603d1de39c9a885d23adea5c18
Arkadiy <arknovichkov@gmail.com>
2026-01-21 20:52:06 +0200

    namespaceName -> NAMESPACE_NAME

--- src/main/java/frc/robot/subsystems/Collection.java
+++ src/main/java/frc/robot/subsystems/Collection.java
@@ -7,17 +7,17 @@ import frc.robot.RobotMap;
 
 public class Collection extends MotoredGenericSubsystem {
 
-    private final static String namespaceName="Collection";
+    private final static String NAMESPACE_NAME ="collection";
 
-    public Collection(String namespaceName, SmartMotorController motorController) {
-        super(namespaceName, motorController);
+    public Collection(String NAMESPACE_NAME, SmartMotorController motorController) {
+        super(NAMESPACE_NAME, motorController);
     }
 
     private static Collection instance;
 
     public static Collection getInstance() {
         if (instance == null) {
-            instance = new Collection(namespaceName,
+            instance = new Collection(NAMESPACE_NAME,
                     new TalonFXWrapper(RobotMap.CAN.COLLECTION_TALON_FX_ID));
         }
         return instance;
