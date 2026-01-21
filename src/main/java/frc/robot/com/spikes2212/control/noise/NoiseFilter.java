42aaf1d379258ee1dd02c3c319deb18cfe1bce80
gil <eingar.gil@gmail.com>
2026-01-21 21:05:05 +0200

    added "spikes lib"

---
+++ src/main/java/frc/robot/com/spikes2212/control/noise/NoiseFilter.java
@@ -0,0 +1,14 @@
+package frc.robot.com.spikes2212.control.noise;
+
+/**
+ * An interface which is implemented by classes that handle noise-filtering.
+ *
+ * @author Simon Kharmatsky
+ */
+public interface NoiseFilter {
+
+    /**
+     * A noise-filtering calculation
+     */
+    double calculate(double measurement);
+}
