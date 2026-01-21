2bb58dc6058e3d54e65f02a428ab01a3dc013fb6
ofek <ofek@ofek-online.com>
2026-01-21 10:49:23 +0200

    added a stop func

--- src/main/java/frc/robot/subsystems/CollectionMovement.java
+++ src/main/java/frc/robot/subsystems/CollectionMovement.java
@@ -44,4 +44,9 @@ public class CollectionMovement extends MotoredGenericSubsystem {
         namespace.putBoolean("bottom limit input", bottomLimit::get);
         namespace.putNumber("motor current speed", motor::getVelocity);
     }
+
+    @Override
+    public void stop() {
+        motor.stopMotor();
+    }
 }
