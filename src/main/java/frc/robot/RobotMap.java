9c1c0551f7c8b11ef9356240ab792992cc165ffa
Arkadiy <arknovichkov@gmail.com>
2026-01-21 21:21:10 +0200

    RobotMap convention thingies

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,7 +3,8 @@ package frc.robot;
 public class RobotMap {
 
     public interface CAN {
-        int COLLECTION_TALON_FX_ID=-1;
+
+        int COLLECTION_TALON_FX_ID= -1;
     }
     
     public interface DIO {
