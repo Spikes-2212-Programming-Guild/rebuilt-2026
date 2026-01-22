15191ce3893d747c51b182f6ed0d82e39c3e58a0
Itay <itay2318@gmail..com>
2026-01-22 06:36:44 +0200

    changed the Robot class to the spikes way

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,8 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
-    public Robot() {
+    @Override
+    public void robotInit() {
 
     }
 
@@ -20,7 +21,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void disabledInit() {
-
+        CommandScheduler.getInstance().cancelAll();
     }
 
     @Override
