6939a9457088b173734b00f974239c151ad25472
Arkadiy <arknovichkov@gmail.com>
2026-01-22 17:28:50 +0200

    cntrl alt l moved Transport to simpleCommands

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,19 +3,19 @@ package frc.robot;
 public class RobotMap {
 
     public interface CAN {
-        int SPINNING_MAGAZINE_TALON_FX_ID =-1;
+        int SPINNING_MAGAZINE_TALON_FX_ID = -1;
     }
-    
+
     public interface DIO {
 
     }
-    
+
     public interface PWM {
 
     }
-    
+
     public interface AIN {
-    
+
     }
 
     public interface PCM {
--- src/main/java/frc/robot/simpleCommands/Transport.java
+++ src/main/java/frc/robot/simpleCommands/Transport.java
@@ -1,12 +1,13 @@
-package frc.robot.commands;
+package frc.robot.simpleCommands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import frc.robot.subsystems.SpinningMagazine;
+
 import java.util.function.Supplier;
 
 public class Transport extends MoveGenericSubsystem {
 
-    public Transport(SpinningMagazine spinningMagazine , Supplier<Double> speed) {
+    public Transport(SpinningMagazine spinningMagazine, Supplier<Double> speed) {
         super(spinningMagazine, speed);
     }
 }
