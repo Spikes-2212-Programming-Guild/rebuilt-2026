415774912180751c62acb690cd6f9026ebb6da62
Arkadiy <arknovichkov@gmail.com>
2026-01-22 17:25:10 +0200

    added a motor TalonFXWrapper changed some motor related thingies the constructor doesnt take a string for namespace name anymore

--- src/main/java/frc/robot/subsystems/SpinningMagazine.java
+++ src/main/java/frc/robot/subsystems/SpinningMagazine.java
@@ -1,28 +1,29 @@
 package frc.robot.subsystems;
 
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
-import com.spikes2212.util.smartmotorcontrollers.SmartMotorController;
 import com.spikes2212.util.smartmotorcontrollers.TalonFXWrapper;
 import frc.robot.RobotMap;
 
 public class SpinningMagazine extends MotoredGenericSubsystem {
 
     private final static String NAMESPACE_NAME = "spinning magazine";
+    private final TalonFXWrapper motor;
 
-    public SpinningMagazine(String NAMESPACE_NAME, SmartMotorController motorController) {
-        super(NAMESPACE_NAME, motorController);
+    public SpinningMagazine(TalonFXWrapper motor) {
+        super(NAMESPACE_NAME, motor);
+        this.motor=motor;
     }
 
     private static SpinningMagazine instance;
 
     public static SpinningMagazine getInstance() {
         if (instance == null) {
-            instance = new SpinningMagazine(NAMESPACE_NAME, new TalonFXWrapper(RobotMap.CAN.SPINNING_MAGAZINE_TALON_FX_ID));
+            instance = new SpinningMagazine(new TalonFXWrapper(RobotMap.CAN.SPINNING_MAGAZINE_TALON_FX_ID));
         }
         return instance;
     }
 
-    // will change in future when it will be decided on what causes it to start/stop
+    //@TODO ask what causes it to start/stop and add it
     @Override
     public boolean canMove(double speed) {
         return super.canMove(speed);
