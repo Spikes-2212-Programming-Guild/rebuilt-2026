01f6c47ac72e876dabb960de1be63fd0b5444e2b
ofek <ofek@ofek-online.com>
2026-01-23 17:08:44 +0200

    wp shooter subsys and command

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -1,13 +1,14 @@
-package commands;
+package frc.robot.commands;
 
 import com.spikes2212.command.genericsubsystem.GenericSubsystem;
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import frc.robot.subsystem.Shooter;
 
 import java.util.function.Supplier;
 
 public class Shoot extends MoveGenericSubsystem {
 
-    public Shoot(GenericSubsystem shooter, Supplier<Double> speedSupplier) {
+    public Shoot(Shooter shooter, Supplier<Double> speedSupplier) {
         super(shooter, speedSupplier);
     }
 }
---
+++ src/main/java/frc/robot/subsystem/Shooter.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystem;
+
+import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
+import com.spikes2212.util.smartmotorcontrollers.TalonFXWrapper;
+import frc.robot.RobotMap;
+
+public class Shooter extends MotoredGenericSubsystem {
+
+    private final TalonFXWrapper upperMotor;
+    private final TalonFXWrapper lowerMotor;
+    static final String NAMESPACE_NAME = "shooter";
+
+    private static Shooter instance;
+
+    public static Shooter getInstance(){
+        if(instance == null){
+            instance = new Shooter(NAMESPACE_NAME,
+                    new TalonFXWrapper(RobotMap.CAN.UPPER_TALON_FX_SHOOTER_ID),
+                    new TalonFXWrapper(RobotMap.CAN.LOWER_TALON_FX_SHOOTER_ID));
+        }
+        return instance;
+    }
+
+    public Shooter(String namespaceName, TalonFXWrapper upperMotor, TalonFXWrapper lowerMotor) {
+        super(namespaceName, upperMotor, lowerMotor);
+        this.upperMotor = upperMotor;
+        this.lowerMotor = lowerMotor;
+    }
+
+    public void calculateAngleToShoot(){
+        //@TODO fill this method when i know the physics of it
+        //the void is intentional so it gives no error when it doesnt return anything
+    }
+}
