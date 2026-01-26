ab38bd5b45d211f9f6003ac679b0d8b8db0dcdf8
or <avilavilavil4@gmail.com>
2026-01-26 20:55:20 +0200

    ctrl alt o in SimpleTransport

--- src/main/java/frc/robot/commands/simpleCommands/SimpleTransport.java
+++ src/main/java/frc/robot/commands/simpleCommands/SimpleTransport.java
@@ -3,8 +3,6 @@ package frc.robot.commands.simpleCommands;
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import frc.robot.subsystems.Transport;
 
-import java.util.function.Supplier;
-
 public class SimpleTransport extends MoveGenericSubsystem {
 
     public SimpleTransport(Transport transport) {
