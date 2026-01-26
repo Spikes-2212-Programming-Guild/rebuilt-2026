0108f92e5881ae7d873cfe052470421927e4e5ce
ofek <ofek@ofek-online.com>
2026-01-26 18:36:42 +0200

    wheel diameter confirmed by yuri and gear ratio confirmed usage by camilia

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -9,13 +9,12 @@ public class Shooter extends SmartMotorControllerGenericSubsystem {
     private static final String NAMESPACE_NAME = "shooter";
 
     private final double GEAR_RATIO = -1;
-    private final double WHEEL_DIAMETER_TO_INCHES = -1;
-    private final double INCHES_TO_METERS = -1;
+    private final double WHEEL_DIAMETER_IN_METERS = 0.1016; //converted 4 inches to meters
 
     private static Shooter instance;
 
     public static Shooter getInstance() {
-        if(instance == null){
+        if (instance == null) {
             instance = new Shooter(NAMESPACE_NAME,
                     new TalonFXWrapper(RobotMap.CAN.SHOOTER_UPPER_TALON_FX_ID),
                     new TalonFXWrapper(RobotMap.CAN.SHOOTER_MIDDLE_TALON_FX_ID),
@@ -35,7 +34,7 @@ public class Shooter extends SmartMotorControllerGenericSubsystem {
         namespace.putNumber("motors", motorController::get);
     }
 
-    public double velocityToMetersPerSecond(){
-        return GEAR_RATIO*WHEEL_DIAMETER_TO_INCHES*INCHES_TO_METERS;
+    public double velocityToMetersPerSecond() {
+        return GEAR_RATIO * WHEEL_DIAMETER_IN_METERS;
     }
 }
