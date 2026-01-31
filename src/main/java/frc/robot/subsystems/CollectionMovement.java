6a1f8962137171f3d0815f1d2a96c9ab345d76cd
or <avilavilavil4@gmail.com>
2026-01-31 10:40:09 +0200

    changed almost all of the subsystem

--- src/main/java/frc/robot/subsystems/CollectionMovement.java
+++ src/main/java/frc/robot/subsystems/CollectionMovement.java
@@ -1,9 +1,10 @@
 package frc.robot.subsystems;
 
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
-import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SmartMotorControllerGenericSubsystem;
 import com.spikes2212.util.smartmotorcontrollers.TalonFXWrapper;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.RobotMap;
 
 public class CollectionMovement extends MotoredGenericSubsystem {
@@ -30,9 +31,16 @@ public class CollectionMovement extends MotoredGenericSubsystem {
     private static final double ABSOLUTE_UPPER_DEG = -1;
     private static final double ABSOLUTE_LOWER_DEG = -1;
     private static final double DEGREES_IN_ROTATIONS = 360;
+    private static final double MOTION_EPSILON = -1.0;
+    private static final double STALL_TIME_LIMIT = -1.0;   // Seconds to wait before triggering stall protection// Minimum degrees change to be considered "moving"
 
-    private final DutyCycleEncoder throughBore;
+    private final DutyCycleEncoder absoluteEncoder;
     private final TalonFXWrapper talonFX;
+    private static DigitalInput bottomLimit;
+    private double lastPositionDegrees = 0;
+    private double lastMoveTime = 0;
+    private boolean isStalled = false;
+
 
     private static CollectionMovement instance;
 
@@ -41,39 +49,64 @@ public class CollectionMovement extends MotoredGenericSubsystem {
             instance = new CollectionMovement(NAMESPACE_NAME,
                     new DutyCycleEncoder(RobotMap.DIO.COLLECTION_MOVEMENT_THROUGH_BORE_ID),
                     new TalonFXWrapper(RobotMap.CAN.COLLECTION_MOVEMENT_TALON_FX_ID),
-                    IS_ENCODER_INVERTED);
+                    IS_ENCODER_INVERTED,bottomLimit);
         }
         return instance;
     }
 
-    private CollectionMovement(String namespaceName, DutyCycleEncoder throughBore,
-                               TalonFXWrapper talonFX, boolean isInverted) {
+    private CollectionMovement(String namespaceName, DutyCycleEncoder absoluteEncoder,
+                               TalonFXWrapper talonFX, boolean isInverted, DigitalInput bottomLimit) {
         super(namespaceName, talonFX);
-        this.throughBore = throughBore;
-        this.throughBore.setInverted(isInverted);
+        this.absoluteEncoder = absoluteEncoder;
+        this.bottomLimit = bottomLimit;
+        this.absoluteEncoder.setInverted(isInverted);
         this.talonFX = talonFX;
         configureDashboard();
     }
 
+    public double getAbsDegrees() {
+        return absoluteEncoder.get();
+    }
+
     public void resetRelativeEncoderRelativeToAbsoluteEncoder() {
-        talonFX.setPosition(throughBore.get());
+        talonFX.setPosition(absoluteEncoder.get());
+    }
+
+    private void checkForStall() {
+        double currentPos = getAbsDegrees();
+        double currentTime = Timer.getFPGATimestamp();
+            double deltaPos = Math.abs(currentPos - lastPositionDegrees);
+
+
+        if (deltaPos > MOTION_EPSILON) {
+            lastMoveTime = currentTime;
+            lastPositionDegrees = currentPos;
+            isStalled = false;
+        }
+
+        else {
+            if (currentTime - lastMoveTime > STALL_TIME_LIMIT) {
+                isStalled = true;
+            }
+        }
     }
 
     @Override
     public boolean canMove(double speed) {
-        if (!(speed < 0 && encoderPositionToDegrees() == ABSOLUTE_UPPER_DEG)) {
-            return true;
-        } else return !(speed > 0 && encoderPositionToDegrees() == ABSOLUTE_LOWER_DEG);
+        if (bottomLimit.get() && speed < 0) {
+            return false;
+        }
+
+        return !(Math.abs(speed) > ABSOLUTE_LOWER_DEG && Math.abs(speed) > ABSOLUTE_UPPER_DEG) || !isStalled;
     }
 
-    public double encoderPositionToDegrees() {
-        return throughBore.get() * DEGREES_IN_ROTATIONS;
-    }
+        public double encoderPositionToDegrees () {
+            return absoluteEncoder.get() * DEGREES_IN_ROTATIONS;
+        }
 
-    @Override
-    public void configureDashboard() {
-        namespace.putNumber("talon relative encoder position", talonFX::getPosition);
-        namespace.putNumber("through bore position", this::encoderPositionToDegrees);
-        namespace.putNumber("motor current speed", talonFX::getVelocity);
+        @Override
+        public void configureDashboard () {
+            namespace.putNumber("talon relative encoder position", talonFX::getPosition);
+            namespace.putNumber("motor current speed", talonFX::getVelocity);
+        }
     }
-}
