850038fba5c36ccf7f61f12bcc06f956de6f5112
ofek <ofek@ofek-online.com>
2026-01-31 10:07:51 +0200

    added DifficultShoot.java

---
+++ src/main/java/frc/robot/commands/difficultcommands/DifficultShoot.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.difficultcommands;
+
+import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.MoveSmartMotorControllerGenericSubsystem;
+import com.spikes2212.control.FeedForwardSettings;
+import com.spikes2212.control.PIDSettings;
+import com.spikes2212.dashboard.RootNamespace;
+import com.spikes2212.util.UnifiedControlMode;
+import frc.robot.subsystems.Shooter;
+
+import java.util.function.Supplier;
+
+public class DifficultShoot extends MoveSmartMotorControllerGenericSubsystem {
+
+    private static final RootNamespace namespace = new RootNamespace("difficult shoot");
+
+    private static final PIDSettings pidSettings = namespace.
+            addPIDNamespace("difficult shoot", PIDSettings.EMPTY_PID_SETTINGS);
+    private static final FeedForwardSettings ffSettings = namespace.
+            addFeedForwardNamespace("difficult shoot", FeedForwardSettings.EMPTY_FF_SETTINGS);
+
+    public DifficultShoot(Shooter shooter, Supplier<Double> setpoint) {
+        super(shooter, pidSettings, ffSettings, UnifiedControlMode.VELOCITY, setpoint, true);
+    }
+}
