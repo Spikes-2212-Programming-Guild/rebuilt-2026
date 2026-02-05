b032f98dc72cc7202655fd45b414d2af3406fd7f
itay <itay2318@gmail.com>
2026-02-05 23:30:05 +0200

    changed the name of the simple spindexter command

--- src/main/java/frc/robot/commands/simplecommands/SimpleSpin.java
+++ src/main/java/frc/robot/commands/simplecommands/SimpleSpin.java
@@ -3,11 +3,9 @@ package frc.robot.commands.simplecommands;
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import frc.robot.subsystems.SpinningMagazine;
 
-import java.util.function.Supplier;
+public class SimpleSpin extends MoveGenericSubsystem {
 
-public class SimpleTransport extends MoveGenericSubsystem {
-
-    public SimpleTransport(SpinningMagazine spinningMagazine) {
+    public SimpleSpin(SpinningMagazine spinningMagazine) {
         super(spinningMagazine, SpinningMagazine.SPEED);
     }
 }
