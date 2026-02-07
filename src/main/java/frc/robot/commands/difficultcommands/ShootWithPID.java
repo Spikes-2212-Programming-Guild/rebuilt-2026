3de169ab52168833e709acf6c253d3bcbf8b11cb
ofek <ofek@ofek-online.com>
2026-02-08 00:19:24 +0200

    changed constants names

--- src/main/java/frc/robot/commands/difficultcommands/ShootWithPID.java
+++ src/main/java/frc/robot/commands/difficultcommands/ShootWithPID.java
@@ -13,13 +13,13 @@ public class ShootWithPID extends MoveSmartMotorControllerGenericSubsystem {
 
     private static final RootNamespace namespace = new RootNamespace("difficult shoot with pid");
 
-    private static final PIDSettings pidSettings = namespace.
+    private static final PIDSettings PID_SETTINGS = namespace.
             addPIDNamespace("difficult shoot", PIDSettings.EMPTY_PID_SETTINGS);
 
-    private static final FeedForwardSettings ffSettings = namespace.
+    private static final FeedForwardSettings FEED_FORWARD_SETTINGS = namespace.
             addFeedForwardNamespace("difficult shoot", FeedForwardSettings.EMPTY_FF_SETTINGS);
 
     public ShootWithPID(Shooter shooter, Supplier<Double> speed) {
-        super(shooter, pidSettings, ffSettings, UnifiedControlMode.VELOCITY, speed, false);
+        super(shooter, PID_SETTINGS, FEED_FORWARD_SETTINGS, UnifiedControlMode.VELOCITY, speed, false);
     }
 }
