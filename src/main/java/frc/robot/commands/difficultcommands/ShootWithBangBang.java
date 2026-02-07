b24f21b2147abc2b9edb43b603b41ab2f440777c
ofek <ofek@ofek-online.com>
2026-02-08 00:12:06 +0200

    changed ShootWithBangBang.java

--- src/main/java/frc/robot/commands/difficultcommands/ShootWithBangBang.java
+++ src/main/java/frc/robot/commands/difficultcommands/ShootWithBangBang.java
@@ -6,7 +6,7 @@ import frc.robot.subsystems.Shooter;
 
 import java.util.function.Supplier;
 
-public class DifficultShootWithBangBang extends MoveGenericSubsystem {
+public class ShootWithBangBang extends MoveGenericSubsystem {
 
     private static final double DEFAULT_TOLERANCE = -1.0;
 
@@ -14,14 +14,14 @@ public class DifficultShootWithBangBang extends MoveGenericSubsystem {
     private final Supplier<Double> setpointSpeed;
     private final BangBangController bangBangController;
 
-    public DifficultShootWithBangBang(Shooter shooter, Supplier<Double> speed, Supplier<Double> tolerance) {
+    public ShootWithBangBang(Shooter shooter, Supplier<Double> speed, Supplier<Double> tolerance) {
         super(shooter, speed);
         this.shooter = shooter;
         this.setpointSpeed = speed;
         this.bangBangController = new BangBangController(tolerance.get());
     }
 
-    public DifficultShootWithBangBang(Shooter shooter, Supplier<Double> speed) {
+    public ShootWithBangBang(Shooter shooter, Supplier<Double> speed) {
         this(shooter, speed, () -> DEFAULT_TOLERANCE);
     }
 
