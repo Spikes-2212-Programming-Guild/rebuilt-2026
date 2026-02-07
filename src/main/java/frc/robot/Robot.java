390fe6ad03f1d8457c9fb048e804d8365078e8ac
ofek <ofek@ofek-online.com>
2026-02-08 00:12:15 +0200

    cleared Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,29 +4,19 @@
 
 package frc.robot;
 
-import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.simplecommands.SimpleShoot;
-import frc.robot.subsystems.Shooter;
-
-import java.util.function.Supplier;
 
 public class Robot extends TimedRobot {
 
-    private final RootNamespace namespace = new RootNamespace("shooter");
-    Shooter shoot = Shooter.getInstance();
-    Supplier<Double> setpoint;
     @Override
     public void robotInit() {
-        setpoint = namespace.addConstantDouble("setpoint", 0.0);
-        namespace.putCommand("shoot", new SimpleShoot(shoot,setpoint));
+
     }
 
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
-        namespace.update();
     }
 
     @Override
