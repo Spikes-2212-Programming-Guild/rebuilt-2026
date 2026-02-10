e6fb5357a15d09fd45958319f1bfe8cc3a97865c
ofek <ofek@ofek-online.com>
2026-02-10 18:11:21 +0200

    added feed forward imp to the ShootWithBangBang.java

--- src/main/java/frc/robot/commands/difficultcommands/ShootWithBangBang.java
+++ src/main/java/frc/robot/commands/difficultcommands/ShootWithBangBang.java
@@ -1,6 +1,9 @@
 package frc.robot.commands.difficultcommands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.control.FeedForwardController;
+import com.spikes2212.control.FeedForwardSettings;
+import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.math.controller.BangBangController;
 import frc.robot.subsystems.Shooter;
 
@@ -8,17 +11,23 @@ import java.util.function.Supplier;
 
 public class ShootWithBangBang extends MoveGenericSubsystem {
 
+    private final RootNamespace namespace = new RootNamespace("shoot with bang bang");
+
     private static final double DEFAULT_TOLERANCE = -1.0;
 
     private final Shooter shooter;
     private final Supplier<Double> setpointSpeed;
     private final BangBangController bangBangController;
+    private final FeedForwardSettings ffSettings = namespace.addFeedForwardNamespace("ff controller",
+            FeedForwardSettings.EMPTY_FF_SETTINGS);
+    private final FeedForwardController ffController;
 
     public ShootWithBangBang(Shooter shooter, Supplier<Double> speed, Supplier<Double> tolerance) {
         super(shooter, speed);
         this.shooter = shooter;
         this.setpointSpeed = speed;
         this.bangBangController = new BangBangController(tolerance.get());
+        ffController = new FeedForwardController(ffSettings);
     }
 
     public ShootWithBangBang(Shooter shooter, Supplier<Double> speed) {
@@ -27,7 +36,9 @@ public class ShootWithBangBang extends MoveGenericSubsystem {
 
     @Override
     public void execute() {
-        double output = bangBangController.calculate(shooter.getVelocity(), setpointSpeed.get());
+        double bangBangOutput = bangBangController.calculate(shooter.getVelocity(), setpointSpeed.get());
+        double ffOutput = ffController.calculate(shooter.getVelocity(), setpointSpeed.get());
+        double output = ffOutput + bangBangOutput;
         shooter.move(output);
     }
 }
