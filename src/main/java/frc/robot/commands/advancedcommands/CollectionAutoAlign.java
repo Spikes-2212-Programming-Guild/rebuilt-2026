559b2fdda9720614059c5545a85bbe9e85a0c2dd
adiY666 <adi.yekutieljr@gmail.com>
2026-02-13 03:33:53 +0200

    created CollectionAutoAlign.java command

---
+++ src/main/java/frc/robot/commands/advancedcommands/CollectionAutoAlign.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.advancedcommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.difficultcommands.CollectionToPosition;
+import frc.robot.commands.simplecommands.SimpleIntake;
+import frc.robot.subsystems.Collection;
+import frc.robot.subsystems.CollectionMovement;
+import frc.robot.commands.difficultcommands.VisionDriveAlignCommand;
+import frc.robot.subsystems.SwerveDrivetrain;
+import java.util.function.Supplier;
+
+public class CollectionAutoAlign extends SequentialCommandGroup {
+
+    public CollectionAutoAlign(Collection collection, CollectionMovement collectionMovement,
+                               SwerveDrivetrain swerve, Supplier<Double> vX,
+                               Supplier<Double> vY) {
+        addCommands(
+                new CollectionToPosition(
+                        collectionMovement, () -> CollectionMovement.CollectionMovementPose.MAX_POSE.neededPose
+                ),
+                new SimpleIntake(collection).alongWith(new VisionDriveAlignCommand(swerve, vX, vY))
+        );
+    }
+}
