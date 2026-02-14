65bb72e4300f617cd9edeb25b99c2143026d5747
itay <itay2318@gmail.com>
2026-02-14 23:03:18 +0200

    changed couple namings in StandardDeviations

--- src/main/java/frc/robot/util/odometry/StandardDeviations.java
+++ src/main/java/frc/robot/util/odometry/StandardDeviations.java
@@ -7,7 +7,8 @@ import edu.wpi.first.math.numbers.N3;
 
 public record StandardDeviations(double translationX, double translationY, double rotation) {
 
-    public static StandardDeviations EMPTY_STD_DEVS = new StandardDeviations(0,0,0);
+    public static StandardDeviations EMPTY_STANDARD_DEVIATIONS
+            = new StandardDeviations(0,0,0);
 
     public Matrix<N3, N1> toMatrix() {
         return VecBuilder.fill(
