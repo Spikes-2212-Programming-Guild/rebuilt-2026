c40a6ccc10acaa4752263df528ce625e3e28b520
Arkadiy <arknovichkov@gmail.com>
2026-02-16 19:37:05 +0200

    i really got no idea how we got here, but its supposed to work

--- src/main/java/frc/robot/commands/advancedcommands/Pass.java
+++ src/main/java/frc/robot/commands/advancedcommands/Pass.java
@@ -1,8 +1,10 @@
 package frc.robot.commands.advancedcommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.difficultcommands.RotateHood;
+import frc.robot.commands.difficultcommands.ShootWithPID;
 import frc.robot.commands.simplecommands.SimpleShoot;
 import frc.robot.commands.simplecommands.SimpleSpin;
 import frc.robot.commands.simplecommands.SimpleTransport;
@@ -14,18 +16,19 @@ import frc.robot.subsystems.Transport;
 import java.util.function.Supplier;
 
 
-public class Pass extends ParallelCommandGroup {
+public class Pass extends SequentialCommandGroup {
 
     public Pass(SpinningMagazine spinningMagazine, Hood hood, Transport transport,
                 Shooter shooter, Hood.HoodPose pose, Supplier<Double> shootingSpeed){
         addCommands(
-                new SimpleShoot(shooter, shootingSpeed),
-                new SequentialCommandGroup(
-                    new RotateHood(hood, pose),
-                    new ParallelCommandGroup(
-                            new SimpleSpin(spinningMagazine),
-                            new SimpleTransport(transport)
-                    )
+                new ParallelCommandGroup(
+                        new RotateHood(hood, pose),
+                        new ShootWithPID(shooter, shootingSpeed)
+                ),
+                new ParallelCommandGroup(
+                    new SimpleSpin(spinningMagazine),
+                    new SimpleTransport(transport),
+                    new SimpleShoot(shooter, shootingSpeed)
                 )
         );
     }
