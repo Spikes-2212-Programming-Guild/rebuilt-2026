4ac6ca47740a2552192d5ebe96de1924f812ff52
Arkadiy <arknovichkov@gmail.com>
2026-02-16 17:13:15 +0200

    pretty much everything

---
+++ src/main/java/frc/robot/commands/advancedcommands/Pass.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.advancedcommands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.difficultcommands.RotateHood;
+import frc.robot.commands.simplecommands.SimpleShoot;
+import frc.robot.commands.simplecommands.SimpleSpin;
+import frc.robot.commands.simplecommands.SimpleTransport;
+import frc.robot.subsystems.Hood;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.SpinningMagazine;
+import frc.robot.subsystems.Transport;
+
+import java.util.function.Supplier;
+
+
+public class Pass extends SequentialCommandGroup {
+
+    public Pass(SpinningMagazine spinningMagazine, Hood hood, Transport transport,
+                Shooter shooter, Hood.HoodPose pose, Supplier<Double> shootingSpeed){
+        addCommands(
+                new RotateHood(hood, pose),
+                new ParallelCommandGroup(
+                        new SimpleSpin(spinningMagazine),
+                        new SimpleTransport(transport),
+                        new SimpleShoot(shooter, shootingSpeed)
+                )
+        );
+    }
+
+
+
+
+}
