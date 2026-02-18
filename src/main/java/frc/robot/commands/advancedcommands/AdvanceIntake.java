7e347f53c872974da10c58305d65ea66034a60b4
Arkadiy <arknovichkov@gmail.com>
2026-02-18 18:18:29 +0200

    everything,

--- src/main/java/frc/robot/commands/advancedcommands/AdvanceIntake.java
+++ src/main/java/frc/robot/commands/advancedcommands/AdvanceIntake.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.advancecommands;
+package frc.robot.commands.advancedcommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.difficultcommands.CollectionToPosition;
