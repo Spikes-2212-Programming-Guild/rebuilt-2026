61da375079adeeb3089a5e768121a649f41ebbad
Arkadiy <arknovichkov@gmail.com>
2026-02-18 17:59:08 +0200

    changed the order in which the Pass takes variables

--- src/main/java/frc/robot/commands/advancedcommands/Pass.java
+++ src/main/java/frc/robot/commands/advancedcommands/Pass.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.advancedcommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.difficultcommands.RotateHood;
 import frc.robot.commands.simplecommands.SimpleShoot;
@@ -15,7 +16,20 @@ import java.util.function.Supplier;
 
 public class Pass extends SequentialCommandGroup {
 
-    public Pass(SpinningMagazine spinningMagazine, Hood hood, Transport transport, Shooter shooter, Hood.HoodPose pose, Supplier<Double> shootingSpeed) {
-        addCommands(new ParallelCommandGroup(new RotateHood(hood, pose), new SimpleShoot(shooter, shootingSpeed)), new ParallelCommandGroup(new SimpleSpin(spinningMagazine), new SimpleTransport(transport), new SimpleShoot(shooter, shootingSpeed)));
+    public Pass(Hood hood, Shooter shooter, Supplier<Double> shootingSpeed,
+                SpinningMagazine spinningMagazine,
+                Transport transport
+                ) {
+        addCommands(
+                new ParallelDeadlineGroup(
+                        new RotateHood(hood, Hood.HoodPose.PASS_ANGLE),
+                        new SimpleShoot(shooter, shootingSpeed)
+                ),
+                new ParallelCommandGroup(
+                        new SimpleSpin(spinningMagazine),
+                        new SimpleTransport(transport),
+                        new SimpleShoot(shooter, shootingSpeed)
+                )
+        );
     }
 }
