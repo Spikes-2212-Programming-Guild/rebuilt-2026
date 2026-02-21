7fb1f1863683157870df1986d7e68cdc18575413
or <avilavilavil4@gmail.com>
2026-02-21 14:59:26 +0200

    changed the name to Collection

--- src/main/java/frc/robot/commands/advancecommands/Collection.java
+++ src/main/java/frc/robot/commands/advancecommands/Collection.java
@@ -3,12 +3,11 @@ package frc.robot.commands.advancecommands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.difficultcommands.CollectionToPosition;
 import frc.robot.commands.simplecommands.SimpleIntake;
-import frc.robot.subsystems.Collection;
 import frc.robot.subsystems.CollectionMovement;
 
-public class AdvanceIntake extends SequentialCommandGroup {
+public class Collection extends SequentialCommandGroup {
 
-    public AdvanceIntake(Collection collection, CollectionMovement collectionMovement) {
+    public Collection(frc.robot.subsystems.Collection collection, CollectionMovement collectionMovement) {
         addCommands(
                 new CollectionToPosition(collectionMovement, () -> CollectionMovement.OPEN_POSE),
                 new SimpleIntake(collection)
