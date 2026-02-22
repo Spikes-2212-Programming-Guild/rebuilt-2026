0c039b86e5c365035f582f3ecd5c174bba2d9442
gil <eingar.gil@gmail.com>
2026-02-22 13:34:10 +0200

    fixed

--- src/main/java/frc/robot/commands/advancecommands/Collect.java
+++ src/main/java/frc/robot/commands/advancecommands/Collect.java
@@ -3,11 +3,12 @@ package frc.robot.commands.advancecommands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.difficultcommands.CollectionToPosition;
 import frc.robot.commands.simplecommands.SimpleIntake;
+import frc.robot.subsystems.Collection;
 import frc.robot.subsystems.CollectionMovement;
 
-public class Collection extends SequentialCommandGroup {
+public class Collect extends SequentialCommandGroup {
 
-    public Collection(frc.robot.subsystems.Collection collection, CollectionMovement collectionMovement) {
+    public Collect(Collection collection, CollectionMovement collectionMovement) {
         addCommands(
                 new CollectionToPosition(collectionMovement, () -> CollectionMovement.OPEN_POSE),
                 new SimpleIntake(collection)
