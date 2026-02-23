9128c5ec448d4540b8d0319ffcc435b54d0f66ff
ofek <ofek@ofek-online.com>
2026-02-23 14:24:26 +0200

    added func to constructor

--- src/main/java/frc/robot/pathplanner/AutonomousContainer.java
+++ src/main/java/frc/robot/pathplanner/AutonomousContainer.java
@@ -74,6 +74,7 @@ public class AutonomousContainer {
         configureAutoBuilder();
         setupTargetPoseUpdateLoop();
         getSelectedCommand();
+        configureAutoChooser();
     }
 
     private void configureAutoBuilder() {
