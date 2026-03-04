399293c6d9e4649cb67061652b3f885f6ac1055a
Spikes2212 <spikes.no2212@gmail.com>
2026-03-04 11:37:08 +0200

    added ids

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,20 +3,20 @@ package frc.robot;
 public class RobotMap {
 
     public interface CAN {
-        
-        int SHOOTER_UPPER_TALON_FX_ID = -1;
-        int SHOOTER_MIDDLE_TALON_FX_ID = -1;
-        int SHOOTER_LOWER_TALON_FX_ID = -1;
 
-        int HOOD_SPARK_MAX = -1;
-      
-        int COLLECTION_TALON_FX_ID = -1;
-      
-        int COLLECTION_MOVEMENT_TALON_FX_ID = -1;
+        int COLLECTION_TALON_FX_ID = 14;
 
-        int SPINNING_MAGAZINE_SPARK_MAX_ID = -1;
+        int COLLECTION_MOVEMENT_TALON_FX_ID = 15;
 
-        int TRANSPORT_SPARK_MAX_ID = -1;
+        int SPINNING_MAGAZINE_TALON_FX_ID = 16;
+
+        int TRANSPORT_SPARK_MAX_ID = 17;
+
+        int HOOD_SPARK_MAX = 18;
+
+        int SHOOTER_LOWER_TALON_FX_ID = 19;
+        int SHOOTER_MIDDLE_TALON_FX_ID = 20;
+        int SHOOTER_UPPER_TALON_FX_ID = 21;
     }
 
     public interface DIO {
