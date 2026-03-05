cf1cd60c8eb827db2ef45087e825e0bb68d62f8a
adiY666 <adi.yekutieljr@gmail.com>
2026-03-05 23:22:23 +0200

    fixed ShootingWithRegression.java

--- src/main/java/frc/robot/commands/advancedcommand/ShootingWithRegression.java
+++ src/main/java/frc/robot/commands/advancedcommand/ShootingWithRegression.java
@@ -1,12 +1,9 @@
 package frc.robot.commands.advancedcommand;
 
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.Constants;
 import frc.robot.commands.difficultcommands.RotateHood;
 import frc.robot.commands.difficultcommands.ShootWithPID;
-import frc.robot.commands.simplecommands.SimpleSpin;
-import frc.robot.commands.simplecommands.SimpleTransport;
 import frc.robot.subsystems.*;
 import frc.robot.utils.ShooterAlgo;
 import java.util.function.Supplier;
@@ -17,34 +14,32 @@ public class ShootingWithRegression extends SequentialCommandGroup {
                                   Transport transport, Supplier<Double> distance) {
         addCommands(
                 Commands.deferredProxy(() -> {
-                    var targetPose = ShooterAlgo.getOptimalPose(distance.get());
+                    Constants.ShooterConstants.FlywheelQuadratic targetPose =
+                            ShooterAlgo.getOptimalPose(distance.get());
                     double targetRPM = ShooterAlgo.calculateRPM(distance.get(), targetPose);
-                    Hood.HoodPose hoodPose = null;
-                    switch (targetPose) {
-                        case SHOOT_POSE1:
-                            hoodPose = Hood.HoodPose.POSE1;
-                        case SHOOT_POSE2:
-                            hoodPose = Hood.HoodPose.POSE2;
-                        case SHOOT_POSE3:
-                            hoodPose = Hood.HoodPose.POSE3;
-                    }
 
+                    Hood.HoodPose hoodPose = switch (targetPose) {
+                        case SHOOT_POSE1 -> Hood.HoodPose.POSE1;
+                        case SHOOT_POSE2 -> Hood.HoodPose.POSE2;
+                        case SHOOT_POSE3 -> Hood.HoodPose.POSE3;
+                    };
 
                     return Commands.sequence(
                             new ParallelCommandGroup(
                                     new RotateHood(hood, hoodPose),
                                     new ShootWithPID(shooter, () -> targetRPM)
                             ),
+
                             new ParallelCommandGroup(
-                                    new SimpleSpin(spinningMagazine),
-                                    new SimpleTransport(transport)
-                            ),
-                            new ParallelCommandGroup(
-                                    Commands.runOnce(shooter::stop, shooter),
-                                    Commands.runOnce(spinningMagazine::stop, spinningMagazine),
-                                    Commands.runOnce(transport::stop, transport)
-                            )
-                    );
+                                    new RunCommand(() -> spinningMagazine.move(SpinningMagazine.SPEED), spinningMagazine),
+                                    new RunCommand(() -> transport.move(Transport.SPEED), transport)
+                            ).withTimeout(1.0)
+
+                    ).finallyDo((interrupted) -> {
+                        shooter.stop();
+                        spinningMagazine.stop();
+                        transport.stop();
+                    });
                 }));
     }
 }
