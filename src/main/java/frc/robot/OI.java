756a15189aa996ee7d93129d01e45885e7a8e3d2
Spikes2212 <spikes.no2212@gmail.com>
2026-03-08 15:44:50 +0200

    OI for collecting

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 import com.spikes2212.util.PlaystationControllerWrapper;
 import frc.robot.commands.advancedcommands.Collect;
-import frc.robot.commands.advancedcommands.Up;
+import frc.robot.commands.advancedcommands.MoveCollectionUp;
 import frc.robot.subsystems.Collection;
 import frc.robot.subsystems.CollectionMovement;
 
@@ -14,7 +14,7 @@ public class OI {
     private final CollectionMovement collectionMovement = CollectionMovement.getInstance();
 
     public OI() {
-        playstationControllerWrapper.getR1Button().whileTrue(new Collect(collection, collectionMovement))
-                .onFalse(new Up(collection, collectionMovement));
-    }
+        playstationControllerWrapper.getL2Button().whileTrue(new Collect(collection, collectionMovement))
+                .onFalse(new MoveCollectionUp(collection, collectionMovement));
+        }
 }
