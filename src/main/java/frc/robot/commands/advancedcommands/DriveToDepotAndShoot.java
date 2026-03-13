19b71124571f2e0bfc0c57a55cb116d8f853f8d9
Itay <itay2318@gmail..com>
2026-03-13 21:20:27 +0200

    added auto commands

---
+++ src/main/java/frc/robot/commands/advancedcommands/DriveToDepotAndShoot.java
@@ -0,0 +1,44 @@
+package frc.robot.commands.advancedcommands;
+
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.swerve.DriveDistanceWithPID;
+import frc.robot.commands.swerve.DriveDistanceWithPID.DriveDirection;
+import frc.robot.commands.swerve.SwerveRotateWithPID;
+import frc.robot.subsystems.forbar.Collection;
+import frc.robot.subsystems.forbar.CollectionMovement;
+import frc.robot.subsystems.shoot.Shooter;
+import frc.robot.subsystems.spindexer.Kicker;
+import frc.robot.subsystems.spindexer.SpinningMagazine;
+import frc.robot.subsystems.swerve.Drivetrain;
+
+/*
+    init_pos: 3.75m, 6m  # in-front of depot
+    init_angle: intake facing other alliance
+ */
+public class DriveToDepotAndShoot extends SequentialCommandGroup {
+
+    public DriveToDepotAndShoot(Drivetrain drivetrain, Shooter shooter,
+                                SpinningMagazine spinningMagazine, Kicker kicker,
+                                Collection collection, CollectionMovement collectionMovement) {
+        addCommands(
+                new DriveDistanceWithPID(drivetrain, -1, DriveDirection.X, false), // drive to shooting position
+                new SwerveRotateWithPID(drivetrain, () -> null, false), // rotate to shoot
+                new AccelerateAndShoot(shooter, () -> null, spinningMagazine, kicker)
+                        .withTimeout(-1), // shoot until all balls are shot
+
+                new SwerveRotateWithPID(drivetrain, () -> 180.0, false), // rotate to face depot
+
+                new ParallelDeadlineGroup( // keep collecting until drove the entire way
+                        new Collect(collection, collectionMovement),
+                        new DriveDistanceWithPID(drivetrain, -2.25, DriveDirection.X, false)
+                ),
+
+                new DriveDistanceWithPID(drivetrain, 2, DriveDirection.X, false), // drive to shooting position
+                new SwerveRotateWithPID(drivetrain, () -> null, false), // rotate to shoot
+                new AccelerateAndShoot(shooter, () -> null, spinningMagazine, kicker)
+                        .withTimeout(-1) // shoot until all balls are shot
+
+        );
+    }
+}
