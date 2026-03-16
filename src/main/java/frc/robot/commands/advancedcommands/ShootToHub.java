8d4127986e35e8491c309c8b0904c591371a8a59
gil <eingar.gil@gmail.com>
2026-03-16 13:22:02 +0200

    fixed names in ShootToHub.java

--- src/main/java/frc/robot/commands/advancedcommands/ShootToHub.java
+++ src/main/java/frc/robot/commands/advancedcommands/ShootToHub.java
@@ -5,15 +5,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.shoot.ShootWithPID;
 import frc.robot.commands.storage.Spin;
 import frc.robot.commands.storage.Transport;
-import frc.robot.commands.swerve.Drive;
-import frc.robot.commands.swerve.RotateAccordingAprilTags;
 import frc.robot.subsystems.shoot.Shooter;
 import frc.robot.subsystems.spindexer.Kicker;
 import frc.robot.subsystems.spindexer.SpinningMagazine;
-import frc.robot.subsystems.swerve.Drivetrain;
 import frc.robot.utils.VisionService;
 
-public class TuneAndShoot extends SequentialCommandGroup {
+public class ShootToHub extends SequentialCommandGroup {
 
     private static final double LINEAR_EQUATION_M_FACTOR = 0.4;
     private static final double LINEAR_EQUATION_B_FACTOR = 2.63;
@@ -22,8 +19,8 @@ public class TuneAndShoot extends SequentialCommandGroup {
     private static final double FIRST_WAIT_TIME = 0.1;
     private static final double SECOND_WAIT_TIME = 10;
 
-    public TuneAndShoot(Shooter shooter, Kicker transport, SpinningMagazine spinningMagazine,
-                        VisionService visionService) {
+    public ShootToHub(Shooter shooter, Kicker kicker, SpinningMagazine spinningMagazine,
+                      VisionService visionService) {
         addCommands(
                 new ShootWithPID(shooter,
                         ()-> (LINEAR_EQUATION_M_FACTOR * (visionService.getZ() + DISTANCE_FROM_CAMERA_TO_SHOOTER) +
@@ -35,7 +32,7 @@ public class TuneAndShoot extends SequentialCommandGroup {
                 },
                 new ParallelCommandGroup(
                         new Spin(spinningMagazine),
-                        new Transport(transport),
+                        new Transport(kicker),
                         new ShootWithPID(shooter,
                                 () -> (LINEAR_EQUATION_M_FACTOR *
                                         (visionService.getZ() + DISTANCE_FROM_CAMERA_TO_SHOOTER) +
