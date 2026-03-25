9269349076a7e8a5e51d4f4c6a6d728821626384
ofek <ofek@ofek-online.com>
2026-03-25 17:22:30 +0200

    changes pinhas made

--- src/main/java/frc/robot/commands/advancedcommands/MoveCollectionUpSlowly.java
+++ src/main/java/frc/robot/commands/advancedcommands/MoveCollectionUpSlowly.java
@@ -6,10 +6,10 @@ import frc.robot.subsystems.forbar.CollectionMovement;
 
 public class MoveCollectionUpSlowly extends SequentialCommandGroup {
 
-    private static final double TIME_TO_MOVE_FAST = 0.25;
+    private static final double TIME_TO_MOVE_FAST = 1.2;
     private static final double TIME_TO_MOVE_SLOW = 1;
-    private static final double MOVE_FAST_SPEED = 0.25;
-    private static final double MOVE_SLOW_SPEED = 0.12;
+    private static final double MOVE_FAST_SPEED = 1;
+    private static final double MOVE_SLOW_SPEED = 0.25;
 
     public MoveCollectionUpSlowly(CollectionMovement collectionMovement) {
         addCommands(
